fa3568be5385b1652596f52188db267b16843ace
jeishu <jeremy.zhu@utexas.edu>
2021-03-18 01:52:29 -0500

    Added a form to the Contacts page.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled from "styled-components";
+import { useForm, ValidationError } from '@formspree/react';
+import styled from 'styled-components';
 
 const ContactsDiv = styled.div`
     display: flex;
@@ -7,14 +8,58 @@ const ContactsDiv = styled.div`
     justify-content: center;
     align-items: center;
     margin: 1rem 2rem;
+    form {
+        display: flex;
+        flex-direction: column;
+        
+        input, label {
+
+        }
+    }
 `;
 
-const Contact = () => {
+const ContactForm = () => {
+    const [state, handleSubmit] = useForm("xjvpnqgl");
+    if (state.succeeded) {
+        return <p>Thank you for contacting me! You will get a reply very soon.</p>;
+    }
     return (
         <ContactsDiv>
-            <h1>Contact</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">
+                    Email Address: 
+                </label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+                <textarea
+                    id="message"
+                    name="message"
+                />
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
+                <button type="submit" disabled={state.submitting}>
+                    Submit
+            </button>
+            </form>
         </ContactsDiv>
-    )
+    );
 }
 
+function Contact() {
+    return (
+        <ContactForm />
+    );
+}
 export default Contact;
